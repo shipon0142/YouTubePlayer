d34476ec65851c2d7140c95a573a625e7378bbcc
shipon0142 <shipon0142@gmail.com>
2019-04-07 11:58:20 +0600

    Initial commit

---
+++ app/src/main/java/com/example/youtubeplayer/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.youtubeplayer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity {
+    YouTubePlayerView  youTubePlayerView;
+
+YouTubePlayer.OnInitializedListener onInitializedListener;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        youTubePlayerView=findViewById(R.id.video1);
+        onInitializedListener=new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+             youTubePlayer.loadPlaylist("PLrrvJVo1_i-fiUCsgtZLsBSS8_3DvXjkN");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+        youTubePlayerView.initialize(YoutubeConfig.getApiKey(),onInitializedListener);
+        startService(new Intent(this, MyService.class));
+    }
+}
---
+++ app/src/main/java/com/example/youtubeplayer/YoutubeConfig.java
@@ -0,0 +1,13 @@
+package com.example.youtubeplayer;
+
+public class YoutubeConfig {
+    public static final String API_KEY = "AIzaSyAoOGZodxouUgfPTZNFMWpEAa3bNRf1NB4";
+
+    public YoutubeConfig() {
+
+    }
+
+    public static String getApiKey() {
+        return API_KEY;
+    }
+}
